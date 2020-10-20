986ca189e51c426226318836a06e350dfe6cceaa
Juliette Rapala <jrapala@gmail.com>
2020-10-19 19:50:24 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Juliette Rapala on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
