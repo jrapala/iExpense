20464766b15836ba7d5f999c3d847385e096ca03
Juliette Rapala <jrapala@gmail.com>
2020-10-26 17:57:24 -0500

    feat: add AddView and present in sheet

---
+++ iExpense/AddView.swift
@@ -0,0 +1,39 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Juliette Rapala on 10/26/20.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    @ObservedObject var expenses: Expenses
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
