660abbfd0514985ad2ae4619a88a9f0e3cc0bec7
Juliette Rapala <jrapala@gmail.com>
2020-10-27 22:10:46 -0500

    feat: add number validation

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -11,6 +11,9 @@ struct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = ""
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
     @ObservedObject var expenses: Expenses
     @Environment(\.presentationMode) var presentationMode
     
@@ -34,7 +37,15 @@ struct AddView: View {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
                     self.presentationMode.wrappedValue.dismiss()
-                    }
+                } else {
+                    alertTitle = "Invalid Amount"
+                    alertMessage = "Please enter a whole number"
+                    showAlert.toggle()
+                }
+            })
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text(alertTitle), message: Text(alertMessage),  dismissButton: .default(Text("OK"))
+                )
             })
         }
     }
