dbd275072ce5d37d965e665d5df3d5e58c923d67
Juliette Rapala <jrapala@gmail.com>
2020-10-27 21:44:00 -0500

    feat: add edit button to ContentView

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                 .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
-            .navigationBarItems(trailing:
+            .navigationBarItems(leading: expenses.items.count > 0 ? EditButton() : nil, trailing:
                 Button(action: {
                     self.showingAddExpense = true
                 }) {
